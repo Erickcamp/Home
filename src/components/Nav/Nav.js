4185c53cae6ff0f3ab5f8e0d6278951dea1bc67c
Erickcamp <thecampkid@gmail.com>
2020-06-17 19:48:28 -0600

    more completed. routes, app done, working on nav next. fixed some spelling

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react'
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+
+const Nav = () => {
+    return(
+        
+    )
+}
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom'
+import Chat from './components/Chat/Chat'
+import Dashboard from './components/Dashboard/Dashboard'
+import Login from './components/Login/Login'
+import Post from './components/Post/Post'
+import Register from './components/Register/Register'
+import View from './components/View/View'
+
+export default (
+    <Switch>
+        <Route exact path='/' component={Login} />
+        <Route path='/chat' component={Chat} />
+        <Route path='/dashboard' component={Dashboard} />
+        <Route path='/post' component={Post} />
+        <Route path='/register' component={Register} />
+        <Route path='/view' component={View} />
+    </Switch>
+)
