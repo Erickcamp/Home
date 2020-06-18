f041dd09563f5ce3cd0df41b2dce2ed54bd32c6d
Erickcamp <thecampkid@gmail.com>
2020-06-17 22:44:25 -0600

    got nav built out, and started redux.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,25 @@
-import React, {useState, useEffect} from 'react'
-import {connect} from 'react-redux'
-import {withRouter} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-const Nav = () => {
-    return(
-        
-    )
-}
+const Nav = (props) => {
+  useEffect(() => {
+    props.getUser();
+  }, []);
+
+  const { push } = props.history;
+  if (props.history === "/") {
+    return null;
+  }
+  return (
+    <nav>
+      <button onClick={() => push("/dashboard")}>Home</button>
+      <button onClick={() => push("/post")}>New Post</button>
+      <button onClick={() => push("/chat")}>Chat</button>
+      <button onClick={() => push("/")}>Logout</button>
+    </nav>
+  );
+};
+
+const mapStateToProps = (reduxState) => reduxState;
+export default connect(mapStateToProps, { getUser })(withRouter(Nav));
---
+++ src/ducks/reducer.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const initialState = {
+    username: '',
+    userId: 0
+}
+
+const LOGIN_USER = 'LOGIN_USER'
+const LOGOUT_USER = 'LOGOUT_USER'
+const GET_USER = 'GET_USER'
+
