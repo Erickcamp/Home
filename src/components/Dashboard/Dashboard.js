f1455bb4c180c73550a6ee5ffc47e4b8210a6769
Erickcamp <thecampkid@gmail.com>
2020-06-23 18:07:44 -0600

    didnt make any real changs, just want this noted to state that my post auth has been tested and works.

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react'
 const Dashboard = () => {
     return(
         <div>
-            This is Dashboard
+            This is Dashboard.
         </div>
     )
 }
